2b6b8079291c30ae35ce7000ecc3d11ee0a08248
QuyTran <75827698+quy1099ht@users.noreply.github.com>
2022-05-14 19:59:17 +0700

    Update connectDB.js

--- data/connectDB.js
+++ data/connectDB.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://quy1099ht:Quypro123@cluster0.icevc.mongodb.net/test", {
+mongoose.connect("mongodb+srv://quy1099ht:******@cluster0.icevc.mongodb.net/test", {
     useNewUrlParser: true
 }, () => {
     console.log("connected")
